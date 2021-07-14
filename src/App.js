d75f5f7c1094f337e1fcd5650182d9d722e7563a
Naman-12911 <namangarg820@gmail.com>
2021-07-14 19:18:29 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App =() =>{
             <h1>hello This is my counter  application</h1>
             <h3>my score is {score}</h3>
 
-            <button className="button1" onClick={()=>{setScore(score+1)}}>Increment +</button>
+            <button className="button1" onClick={()=>(score<25)?setScore(score+1):""}>Increment +</button>
             <button className="button2" onClick={()=>(score>0)?setScore(score-1):"" }> Decrement -</button>
             <button  className="button3" onClick={()=>{setScore(0)}}> Reset </button>
         </div>
